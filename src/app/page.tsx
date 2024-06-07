8adc98e2855407309a8a11c8ff9def4eaab27ce4
donovancastaneda <donovandcastanedajr@gmail.com>
2024-06-07 17:27:22 -0500

    fix: auth form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <SignInForm/>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="relative bg-white shadow-xl rounded-3xl p-20 bg-clip-padding border border-gray-300 backdrop-blur-lg backdrop-filter">
+        <SignInForm />
       </div>
     </main>
   );
